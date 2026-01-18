8a2efaf6b3b366d899d59e274f0fbdf2e4c200da
Dappy26 <makindedapo02@gmail.com>
2026-01-18 18:25:18 -0500

    Add Basic_HTML_andJavaScript_1folder

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,5 @@
+function Show_Message() {
+    let text = "JavaScript makes webpages come alive!";
+    let coloredText = text.fontcolor("blue");
+    document.getElementById("MessageArea").innerHTML = coloredText;
+}
